096236a84c0081e15f913c58ad2893bcccd5dd86
lijaschopka <123467927+lijaschopka@users.noreply.github.com>
2023-02-15 15:14:22 +0000

    added dependency

--- src/main/java/is/hi/egm8/hbv202g/App.java
+++ src/main/java/is/hi/egm8/hbv202g/App.java
@@ -1,13 +1,14 @@
 package is.hi.egm8.hbv202g;
+import org.apache.commons.numbers.primes.Primes;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(Primes.nextPrime(123456789));
     }
 }
